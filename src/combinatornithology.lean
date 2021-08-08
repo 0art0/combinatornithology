2556632baf96ee1ac137effe5214619ecd94c5dd
0Art0 <anand.rao.art@gmail.com>
2021-08-08 13:39:04 +0530

    final

--- src/combinatornithology.lean
+++ src/combinatornithology.lean
@@ -13,7 +13,7 @@ namespace enchantedforest
 
     -- all birds in the enchanted forest belong to the type `Bird`
     -- a `Type` can be thought of as being similar to a `Set`
-    constant Bird : Type 
+    constant Bird : Type
 
     /-
     Given any birds `A` and `B`, if the name of bird `B` is called out to `A`, 
@@ -26,11 +26,12 @@ namespace enchantedforest
     -- better notation for denoting response
     -- the operator is left-associative
     -- the ` ◁ ` symbol (typed as `\lhd`) resembles an ear/beak
-    infix ` ◁ `:100 := response
+    infix ` ◁ ` : 100 := response
 
   end introduction
 
   open introduction
+
 
   namespace identitybird
     /-
@@ -45,9 +46,10 @@ namespace enchantedforest
 
     -- the definition of the identity bird
     constant I : Bird
-    constant I.call : ∀ (x : Bird), (I ◁ x) = x
+    constant I.call : ∀ x : Bird, (I ◁ x) = x
     
   end identitybird
+
 
   /-
   A bird `A` is said to be *fond* of another bird `B` if
@@ -62,7 +64,7 @@ namespace enchantedforest
   -/
 
   -- the definition of egocentricity
-  notation [parsing_only] E ` is_egocentric ` := E ◁ E = E
+  notation [parsing_only] E ` is_egocentric` := E ◁ E = E
 
   section defending_the_identitybird
     open identitybird
@@ -78,13 +80,13 @@ namespace enchantedforest
     -- The identity bird is fond of every bird.
     theorem identity_fond_of_all : ∀ x : Bird, I is_fond_of x :=
     begin
-      exact I.call,
+      sorry
     end
 
     -- The identity bird is egocentric.
     theorem identity_egocentric : I is_egocentric :=
     begin
-      exact identity_fond_of_all I,
+      sorry
     end
 
     /-
@@ -101,7 +103,7 @@ namespace enchantedforest
   -/
 
   -- the definition of hopeless egocentricity
-  notation [parsing_only] B ` is_hopelessly_egocentric ` := ∀ x, B ◁ x = B
+  notation [parsing_only] B ` is_hopelessly_egocentric` := ∀ x, B ◁ x = B
 
   /-
   More generally, a bird `A` is *fixated* on a bird `B` if
@@ -112,6 +114,7 @@ namespace enchantedforest
 
   -- the definition of fixatedness
   notation [parsing_only] A ` is_fixated_on ` B := ∀ x, A ◁ x = B
+
 
   namespace kestrel
     /-
@@ -130,11 +133,18 @@ namespace enchantedforest
   section kestrel_theorems
     open kestrel
 
+    -- For any bird `x`, the bird `K ◁ x` is fixated on `x`.
+    theorem k_x_fixated_on_x : ∀ x, K ◁ x is_fixated_on x :=
+    begin
+      sorry
+    end
+
     -- An egocentric kestrel must be hopelessly egocentric.
-    theorem kestrel_egocentrism (kestrel_egocentric : K is_egocentric) : 
+    theorem kestrel_egocentrism 
+      (kestrel_egocentric : K is_egocentric) : 
       (K is_hopelessly_egocentric) :=
     begin
-      sorry,
+      sorry
     end
 
     -- The left cancellation law for kestrels.
@@ -143,10 +153,10 @@ namespace enchantedforest
       (kestrel_application : (K ◁ x) = (K ◁ y)) :
       (x = y) :=
     begin
-      sorry,
+      sorry
     end
 
-    -- For an arbitrary bird `x`, if `K` is fond of `K ◁ x`,
+    -- `*` For an arbitrary bird `x`, if `K` is fond of `K ◁ x`,
     -- then `K` is fond of `x`.
     theorem kestrel_fondness 
       (x : Bird)
@@ -180,18 +190,19 @@ namespace enchantedforest
       (I_agreeable : I is_agreeable) :
       ∀ B, ∃ x, B is_fond_of x :=
     begin
-      sorry,
+      sorry
     end
 
-    -- If every bird is fond of at least one bird, then
+    -- `*` If every bird is fond of at least one bird, then
     -- the identity bird must be agreeable.
     theorem fondness_induces_agreeable_identity
       (all_birds_fond : ∀ B, ∃ x, B is_fond_of x) :
       I is_agreeable :=
     begin
-      sorry,
+      sorry
     end
   end identitybird_theorems
+
 
   namespace mockingbird
     /-
@@ -207,7 +218,7 @@ namespace enchantedforest
 
   end mockingbird
 
-  namespace forestcompositionaxiom
+  namespace forestcompositionlaw
     /-
     Given any birds `A`, `B`, `C`, the bird `C` is said to *compose* 
     `A` with `B` if for every bird `x`, the following condition holds
@@ -224,7 +235,7 @@ namespace enchantedforest
     -- the definition of composition
     axiom composition (A B : Bird) : ∀ {x : Bird}, (A ∘ B) ◁ x = A ◁ (B ◁ x)
 
-  end forestcompositionaxiom
+  end forestcompositionlaw
 
   section mockingbird_theorems
     open mockingbird 
@@ -232,10 +243,10 @@ namespace enchantedforest
     -- The mockingbird is agreeable.
     theorem mockingbird_agreeable : M is_agreeable :=
     begin
-      sorry,
+      sorry
     end
 
-    open forestcompositionaxiom
+    open forestcompositionlaw
 
     -- If a mockingbird is in the forest and the composition law holds, 
     -- then every bird is fond of at least one bird.
@@ -243,7 +254,7 @@ namespace enchantedforest
     begin
       intro A,
 
-      existsi ((A ∘ M) ◁ (A ∘ M)), -- this is similar to the Y-combinator
+      existsi ((A ∘ M) ◁ (A ∘ M)),
 
       conv
       begin
@@ -257,6 +268,22 @@ namespace enchantedforest
     theorem exists_egocentric : ∃ E, E is_egocentric :=
     begin
       sorry,
+    end
+
+    -- `*` If `A` is an agreeable bird and the composition law holds,
+    -- every bird is fond of at least one bird.
+    theorem agreeability_induces_fondness 
+      (α : Bird)
+      (α_agreeable : α is_agreeable)
+      : ∀ A, ∃ B, A is_fond_of B :=
+    begin
+      sorry
+    end
+
+    -- A proof of the earlier theorem as a corollary of the previous one.
+    theorem mockingbird_agreeable_fondness : ∀ A, ∃ B, A is_fond_of B :=
+    begin
+      exact (agreeability_induces_fondness M mockingbird_agreeable)
     end
 
   end mockingbird_theorems
@@ -279,13 +306,13 @@ namespace enchantedforest
 
   section bluebird_theorems
     open bluebird
-    open forestcompositionaxiom
+    open forestcompositionlaw
 
     -- The bluebird is capable of composing one bird with another.
     theorem bluebird_composition (x y : Bird) :
       ∀ z : Bird, ((B ◁ x) ◁ y) ◁ z = (x ∘ y) ◁ z :=
     begin
-      sorry,
+      sorry
     end
 
     open mockingbird
@@ -295,7 +322,7 @@ namespace enchantedforest
     -- bird `β` using bird calls such that `A` is fond of `β`.
     theorem all_birds_fond : ∀ A, ∃ β, A is_fond_of β :=
     begin
-      sorry,
+      sorry
     end
 
   end bluebird_theorems
@@ -315,6 +342,19 @@ namespace enchantedforest
 
   end lark
 
+  namespace lark_theorems
+    open lark
+
+    -- `*` Every bird is fond of a hopelessly egocentric lark.
+    theorem egocentric_lark_popular
+      (egocentric_lark : L is_egocentric) :
+      ∀ β, β is_fond_of L :=
+    begin
+      sorry
+    end
+    
+  end lark_theorems
+
   namespace starling
     /-
       A *starling* is a bird `S` that satisfies the following condition
@@ -328,6 +368,26 @@ namespace enchantedforest
     constant S.call : ∀ (x y z : Bird), (((S ◁ x) ◁ y) ◁ z) = (x ◁ z) ◁ (y ◁ z)
   end starling
 
+  section starling_theorems
+    open starling
+    open kestrel
+
+    /-
+      The existence of a Starling and a Kestrel in the forest is
+      sufficient to imply the existence of several other birds.
+    -/
+
+    -- Derive the identity bird.
+    
+
+    -- Derive the mockingbird.
+
+
+    -- `*` Derive the bluebird.
+
+
+  end starling_theorems
+
   section summoning_a_sagebird
     /-
       According to folklore, a *sagebird* or an *oracle bird* `Θ` is
@@ -339,11 +399,11 @@ namespace enchantedforest
     -/
   
     -- adding all the known birds
-    open mockingbird
-    open kestrel
     open identitybird
-    open lark
+    open kestrel
+    open mockingbird
     open bluebird
+    open lark
     open starling
 
     -- a sage bird exists in the forest
@@ -362,6 +422,34 @@ end enchantedforest
     All birds can be derived from just the 
     kestrel (`K`) and the
     starling (`S`)!
+
+    # The algorithm
+
+    Define the `α-eliminate` of an expression `E` to be
+    an expression `F` such that `F α = E`.
+    1. The α-eliminate of `α` is `I`.
+    2. If `α` does not occur in `E`, then `K E` is the
+        α-eliminate.
+    3. If `E` is of the form `F α`, then `F` is the
+        α-eliminate of `E`.
+    4. If `E = F G`, and `F'` and `G'` are the corresponding
+      α-eliminates of the expressions, then 
+          `S (F') (G')` is the corresponding α-eliminate.
+
+    Repeated α-elimination of all the variables involved gives the
+    expression for the bird in terms of `S` and `K`.
+
+    For a recursive bird `U` (i.e., a bird whose call depends on itself),
+    replace every occurrence of the letter `U` in the call with an unused
+    variable name and solve as above. 
+    
+    Call this modified bird `V`. It satisfies the property `V ◁ U = U`.
+    
+    Since the existence of the mockingbird and the bluebird is sufficient to 
+    guarantee that every bird is fond of some bird, one can find a bird `F` such that
+    `V` is fond of `F`, that is, `V ◁ F = F`. But this is the property that `U` is required
+    to satisfy. Thus `F`, which can be derived from `S` and `K` using the sage bird,
+    is the required bird.
   -/
 
 -- TO-DO
@@ -377,4 +465,5 @@ end avianarithmetic
   1. "To Mock a Mockingbird", by Raymond Smullyan (https://en.wikipedia.org/wiki/To_Mock_a_Mockingbird)
   2. "To Dissect a Mockingbird", by David Keenan (https://dkeenan.com/Lambda/index.htm)
   3. SKI Combinator calculus (https://en.wikipedia.org/wiki/SKI_combinator_calculus)
+  4. The Natural Number Game (https://www.ma.imperial.ac.uk/~buzzard/xena/natural_number_game/)
 -/
